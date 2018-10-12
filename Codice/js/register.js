cac8996caf3a5e65847e74f509b84c7b2487ae56
matteoforni <matteo.forni@samtrevano.ch>
2018-10-12 16:26:27 +0200

    Modifica del codice e aggiunta del diario

--- Codice/js/register.js
+++ Codice/js/register.js
@@ -1,5 +1,80 @@
-function textControl(input, text){
-	if(/[^A-Za-züèöéàä]/.test(text)){
-		document.getElement(input).style.borderColor = "red";
+function textValidation(id, text){
+	var regex = /^[a-zA-Z àáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĆČĖĘÈÉÊËÌÍÎÏĮŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑČŠŽ]+$/;
+	if(regex.test(text) && text.length <= 50){
+		document.getElementById("avanti").disabled = true;
+		document.getElementById(id).style.borderColor = "rgb(180,180,180)";
+	}else {
+		document.getElementById("avanti").disabled = false;
+		document.getElementById(id).style.borderColor = "red";
 	}
+}
+
+function capValidation(id, text){
+	var regex = /^[0-9]+$/;
+	if(regex.test(text && text.length <= 5)){
+		document.getElementById("avanti").disabled = false;
+		document.getElementById(id).style.borderColor = "rgb(180,180,180)";
+	}else{
+		document.getElementById("avanti").disabled = true;
+		document.getElementById(id).style.borderColor = "red";
+	}
+}
+
+function dateValidation(id, date){
+	var today = Date.now();
+	date = new Date(date);
+	var millis = date.getTime();
+	if(millis>today){
+		document.getElementById("avanti").disabled = true;
+		document.getElementById(id).style.borderColor = "red";
+	}else{
+		document.getElementById("avanti").disabled = false;
+		document.getElementById(id).style.borderColor = "rgb(180,180,180)";
+	}
+}
+
+function civicNumberValidation(id, text){
+	var regex = /^[0-9a-zA-Z]+$/;
+	if(regex.test(text && text.length <= 4)){
+		document.getElementById("avanti").disabled = false;
+		document.getElementById(id).style.borderColor = "rgb(180,180,180)";
+	}else{
+		document.getElementById("avanti").disabled = true;
+		document.getElementById(id).style.borderColor = "red";
+	}
+}
+
+function checkLength(id, text){
+	if(text.length <= 500){
+		document.getElementById("avanti").disabled = true;
+		document.getElementById(id).style.borderColor = "rgb(180,180,180)";
+	}else {
+		document.getElementById("avanti").disabled = false;
+		document.getElementById(id).style.borderColor = "red";
+	}
+}
+
+function phoneValidator(id, text){
+	var regex = /^[0-9 +-]+$/;
+	if(regex.test(text && text.length < 30 && text.length >= 10)){
+		document.getElementById("avanti").disabled = false;
+		document.getElementById(id).style.borderColor = "rgb(180,180,180)";
+	}else{
+		document.getElementById("avanti").disabled = true;
+		document.getElementById(id).style.borderColor = "red";
+	}
+
+}
+
+function disableInputs(){
+	document.getElementById("nome").disabled = true;
+	document.getElementById("cognome").disabled = true;
+	document.getElementById("data").disabled = true;
+	document.getElementById("E-Mail").disabled = true;
+	document.getElementById("CAP").disabled = true;
+	document.getElementById("No-Civico").disabled = true;
+	document.getElementById("via").disabled = true;
+	document.getElementById("telefono").disabled = true;
+	document.getElementById("hobby").disabled = true;
+	document.getElementById("professione").disabled = true;
 }
--- Codice/register.php
+++ Codice/register.php
@@ -10,63 +10,76 @@
   	<link rel="stylesheet" type="text/css" href="css/register.css">
   	<link rel="stylesheet" type="text/css" href="css/animate.css-master/animate.min.css">
 
-  	<script type="text/javascript" src="js/register.js"></script>
+  	
 </head>
 <body>
-	<h1 class="animated slideInDown	">Registrati</h1>
-	<table id="mainForm">
-		<tr class="animated slideInRight">
-			<td><h5>Nome</h5></td>
-			<td><input type="text" name="nome" title="nome" id="cognome" onblur="textControl(this.id, this.content)"></td>
-		</tr>
-		<tr class="animated slideInLeft">
-			<td><h5>Cognome</h5></td>
-			<td><input type="text" name="cognome" title="cognome" id="cognome"></td>
-		</tr>
-		<tr class="animated slideInRight">
-			<td><h5>Data di nascita</h5></td>
-			<td><input type="date" name="data" id="data"></td>
-		</tr>
-		<tr class="animated slideInLeft">
-			<td><h5>E-Mail</h5></td>
-			<td><input type="text" name="E-Mail" title="E-Mail" id="E-Mail"></td>
-		</tr>
-		<tr class="animated slideInRight">
-			<td><h5>CAP</h5></td>
-			<td><input type="text" name="CAP" title="CAP" id="CAP"></td>
-		</tr>
-		<tr class="animated slideInLeft">
-			<td><h5>Num. Civico</h5></td>
-			<td><input type="text" name="telefono" title="telefono" id="telefono"></td>
-		</tr>
-		<tr class="animated slideInRight">
-			<td><h5>Via</h5></td>
-			<td><input type="text" name="via" title="via" id="via"></td>
-		</tr>	
-		<tr class="animated slideInLeft">
-			<td><h5>Telefono</h5></td>
-			<td><input type="text" name="telefono" title="telefono" id="telefono"></td>
-		</tr>	
-		<tr class="animated slideInRight">
-			<td><h5>Sesso</h5></td>
-			<td><input type="radio" name="sesso" value="uomo">Uomo
-			<input type="radio" name="sesso" value="donna">Donna</td>
-		</tr>	
-		<tr class="animated slideInLeft">
-			<td><h5>Hobby</h5></td>
-			<td><input type="text" name="hobby" title="hobby" id="hobby"></td>
-		</tr>
-		<tr class="animated slideInRight">
-			<td><h5>Professione</h5></td>
-			<td><input type="text" name="professione" title="professione" id="professione"></td>
-		</tr>
-		<tr>
-			<td class="animated slideInLeft"><input type="button" name="avanti" value="Avanti" id="avanti"></td>
-			<td class="animated slideInRight"><input type="button" name="correggi" value="Cancella"></td>
-		</tr>	
-	</table>
+	<div id="mainForm">
+		<h1 class="animated slideInDown">Registrati</h1>
+		<form method="POST" action="<?php htmlspecialchars($_SERVER["PHP_SELF"])?>">
+			<table id="mainTable">
+				<tr>
+					<td><h5>Nome</h5></td>
+					<td><input type="text" name="nome" title="nome" id="nome" 
+					required="true" onblur="textValidation(this.id, this.value)"></td>
+				</tr>
+				<tr>
+					<td><h5>Cognome</h5></td>
+					<td><input type="text" name="cognome" title="cognome" id="cognome" required="true" onblur="textValidation(this.id, this.value)"></td>
+				</tr>
+				<tr>
+					<td><h5>Data di nascita</h5></td>
+					<td><input type="date" name="data" id="data" required="true" 
+						onblur="dateValidation(this.id, this.value)"></td>
+				</tr>
+				<tr>
+					<td><h5>E-Mail</h5></td>
+					<td><input type="text" name="E-Mail" title="E-Mail" id="E-Mail" required="true"></td>
+				</tr>
+				<tr>
+					<td><h5>CAP</h5></td>
+					<td><input type="text" name="CAP" title="CAP" id="CAP" required="true" 
+					onblur="capValidation(this.id, this.value)"></td>
+				</tr>
+				<tr>
+					<td><h5>Num. Civico</h5></td>
+					<td><input type="text" name="No-Civico" title="No-Civico" id="No-Civico" required="true" onblur="civicNumberValidation(this.id, this.value)"></td>
+				</tr>
+				<tr>
+					<td><h5>Via</h5></td>
+					<td><input type="text" name="via" title="via" id="via" required="true" onblur="textValidation(this.id, this.value)"></td>
+				</tr>	
+				<tr>
+					<td><h5>Telefono</h5></td>
+					<td><input type="text" name="telefono" title="telefono" id="telefono" required="true"></td	>
+				</tr>	
+				<tr>
+					<td><h5>Sesso</h5></td>
+					<td><input type="radio" name="sesso" value="uomo">Uomo
+					<input type="radio" name="sesso" value="donna">Uomo
+					<input type="radio" name="sesso" value="altro">Altro</td>
+				</tr>	
+				<tr>
+					<td><h5>Hobby</h5></td>
+					<td><input type="text" name="hobby" title="hobby" id="hobby" onblur="checkLenght(this.id, this.value)"></td>
+				</tr>
+				<tr>
+					<td><h5>Professione</h5></td>
+					<td><input type="text" name="professione" title="professione" id="	professione"onblur="checkLenght(this.id, this.value)"></td>
+				</tr>
+				<tr>
+					<td><input type="submit" name="avanti" value="Avanti" id="avanti" onclick="disableInputs()"></td>
+					<td><a href="register.php"><input type="button" name="correggi" value="Cancella"></a></td>
+				</tr>	
+			</table>
+		</form>
+	</div>
 	<?php
-
+		if($_SERVER["REQUEST_METHOD"] == "POST"){
+			if(isset($_POST["nome"]) && isset($_POST["cognome"]) && isset($_POST["data"]) && isset($_POST["E-Mail"])){
+				
+			}
+		}
 	?>
+	<script type="text/javascript" src="js/register.js"></script>
 </body>
 </html>
