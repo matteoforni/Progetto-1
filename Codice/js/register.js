3a307b2f5735e281b28977b440cc05cece8d9348
matteoforni <matteo.forni@samtrevano.ch>
2018-10-17 14:42:45 +0200

    Modificato il codice e aggiunto il diario

--- Codice/js/register.js
+++ Codice/js/register.js
@@ -1,22 +1,31 @@
+var control = false;
 function textValidation(id, text){
 	var regex = /^[a-zA-Z àáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĆČĖĘÈÉÊËÌÍÎÏĮŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑČŠŽ]+$/;
 	if(regex.test(text) && text.length <= 50){
-		document.getElementById("avanti").disabled = true;
-		document.getElementById(id).style.borderColor = "rgb(180,180,180)";
+		document.getElementById(id).style.borderColor = "green";
+		control = true;	
+		removeErrorMessage(id);
+		return true;
 	}else {
-		document.getElementById("avanti").disabled = false;
+		control = false;	
 		document.getElementById(id).style.borderColor = "red";
+		createErrorMessage(id);
+		return false;
 	}
 }
 
 function capValidation(id, text){
 	var regex = /^[0-9]+$/;
-	if(regex.test(text && text.length <= 5)){
-		document.getElementById("avanti").disabled = false;
-		document.getElementById(id).style.borderColor = "rgb(180,180,180)";
+	if(regex.test(text) && text.length <= 5){
+		document.getElementById(id).style.borderColor = "green";
+		control = true;	
+		removeErrorMessage(id);
+		return true;
 	}else{
-		document.getElementById("avanti").disabled = true;
 		document.getElementById(id).style.borderColor = "red";
+		control = false;
+		createErrorMessage(id);
+		return false;
 	}
 }
 
@@ -25,56 +34,150 @@ function dateValidation(id, date){
 	date = new Date(date);
 	var millis = date.getTime();
 	if(millis>today){
-		document.getElementById("avanti").disabled = true;
 		document.getElementById(id).style.borderColor = "red";
+		control = false;
+		createErrorMessage(id);
+		return false;
 	}else{
-		document.getElementById("avanti").disabled = false;
-		document.getElementById(id).style.borderColor = "rgb(180,180,180)";
+		document.getElementById(id).style.borderColor = "green";
+		control = true;	
+		removeErrorMessage(id);
+		return true;
 	}
 }
 
 function civicNumberValidation(id, text){
 	var regex = /^[0-9a-zA-Z]+$/;
-	if(regex.test(text && text.length <= 4)){
-		document.getElementById("avanti").disabled = false;
-		document.getElementById(id).style.borderColor = "rgb(180,180,180)";
+	if(regex.test(text) && text.length <= 4){
+		document.getElementById(id).style.borderColor = "green";
+		control = true;	
+		removeErrorMessage(id);
+		return true;
 	}else{
-		document.getElementById("avanti").disabled = true;
 		document.getElementById(id).style.borderColor = "red";
+		control = false;
+		createErrorMessage(id);
+		return false;
 	}
 }
 
 function checkLength(id, text){
 	if(text.length <= 500){
-		document.getElementById("avanti").disabled = true;
-		document.getElementById(id).style.borderColor = "rgb(180,180,180)";
+		document.getElementById(id).style.borderColor = "green";
+		control = true;	
+		removeErrorMessage(id);
+		return true;
 	}else {
-		document.getElementById("avanti").disabled = false;
 		document.getElementById(id).style.borderColor = "red";
+		control = false;
+		createErrorMessage(id);
+		return false;
 	}
 }
 
 function phoneValidator(id, text){
 	var regex = /^[0-9 +-]+$/;
-	if(regex.test(text && text.length < 30 && text.length >= 10)){
-		document.getElementById("avanti").disabled = false;
-		document.getElementById(id).style.borderColor = "rgb(180,180,180)";
+	if(regex.test(text) && text.length < 30 && text.length >= 10){
+		document.getElementById(id).style.borderColor = "green";
+		control = true;	
+		removeErrorMessage(id);
+		return true;
 	}else{
-		document.getElementById("avanti").disabled = true;
 		document.getElementById(id).style.borderColor = "red";
+		control = false;
+		createErrorMessage(id);
+		return false;
 	}
 
 }
 
+function emailValidator(id, text){
+	var regex = /^([A-Za-z0-9._%+-])+@([A-Za-z0-9.-])+\.([A-Za-z]){2,4}/;
+	if(regex.test(text) && text.length < 50){
+		document.getElementById(id).style.borderColor = "green";
+		control = true;	
+		removeErrorMessage(id);
+		return true;
+	}else{
+		document.getElementById(id).style.borderColor = "red";
+		control = false;
+		createErrorMessage(id);
+		return false;
+	}
+
+}
+
+function createErrorMessage(id){
+	var div = document.createElement("div");
+	div.style.background = "white";
+	div.id = "error-" + id;
+	div.style.borderRadius = "10px";
+	div.style.width = "12%";
+	div.style.height = "12%";
+	div.style.zIndex = "2";	
+	var t = document.createTextNode("Il campo " + id + " è sbagliato");
+    div.appendChild(t);
+    document.body.insertBefore(div, document.body.firstChild);
+}
+
+function removeErrorMessage(id){
+	if(error){
+		var error = "error-" + id;
+		var text = document.getElementById(error);
+    	document.getElementById("mainForm").removeChild(text);
+	}	
+}
+
+function enableButton(){
+	if(textValidation() && capValidation() && emailValidator() && phoneValidator() && civicNumberValidation() && dateValidation()){
+		var button = document.getElementById("avanti");
+		button.style.disabled = "false";
+		button.addEventListener("click", disableInputs()); 
+	}
+}
+
 function disableInputs(){
-	document.getElementById("nome").disabled = true;
-	document.getElementById("cognome").disabled = true;
-	document.getElementById("data").disabled = true;
-	document.getElementById("E-Mail").disabled = true;
-	document.getElementById("CAP").disabled = true;
-	document.getElementById("No-Civico").disabled = true;
-	document.getElementById("via").disabled = true;
-	document.getElementById("telefono").disabled = true;
-	document.getElementById("hobby").disabled = true;
-	document.getElementById("professione").disabled = true;
+	if(textValidation() && capValidation() && emailValidator() && phoneValidator() && civicNumberValidation() && dateValidation()){
+		document.getElementById("nome").disabled = true;
+		document.getElementById("cognome").disabled = true;
+		document.getElementById("data").disabled = true;
+		document.getElementById("E-Mail").disabled = true;
+		document.getElementById("CAP").disabled = true;
+		document.getElementById("No-Civico").disabled = true;
+		document.getElementById("via").disabled = true;
+		document.getElementById("telefono").disabled = true;
+		document.getElementById("hobby").disabled = true;
+		document.getElementById("professione").disabled = true;
+
+		var button = document.getElementById("cancella");
+		button.value = "correggi";
+		button.addEventListener("click", enableInputs()); 
+	}else{
+		enableOverlay();
+	}
+}
+
+function enableInputs(){
+	document.getElementById("nome").disabled = false;
+	document.getElementById("cognome").disabled = false;
+	document.getElementById("data").disabled = false;
+	document.getElementById("E-Mail").disabled = false;
+	document.getElementById("CAP").disabled = false;
+	document.getElementById("No-Civico").disabled = false;
+	document.getElementById("via").disabled = false;
+	document.getElementById("telefono").disabled = false;
+	document.getElementById("hobby").disabled = false;
+	document.getElementById("professione").disabled = false;
+
+	var button = document.getElementById("cancella");
+	button.value = "cancella";
+	button.addEventListener("click", disableInputs()); 
+}
+
+function enableOverlay() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+function disableOverlay() {
+    document.getElementById("overlay").style.display = "none";
 }
--- Codice/register.php
+++ Codice/register.php
@@ -18,57 +18,58 @@
 		<form method="POST" action="<?php htmlspecialchars($_SERVER["PHP_SELF"])?>">
 			<table id="mainTable">
 				<tr>
-					<td><h5>Nome</h5></td>
+					<td><h5>Nome *</h5></td>
 					<td><input type="text" name="nome" title="nome" id="nome" 
 					required="true" onblur="textValidation(this.id, this.value)"></td>
 				</tr>
 				<tr>
-					<td><h5>Cognome</h5></td>
+					<td><h5>Cognome *</h5></td>
 					<td><input type="text" name="cognome" title="cognome" id="cognome" required="true" onblur="textValidation(this.id, this.value)"></td>
 				</tr>
 				<tr>
-					<td><h5>Data di nascita</h5></td>
+					<td><h5>Data di nascita *</h5></td>
 					<td><input type="date" name="data" id="data" required="true" 
 						onblur="dateValidation(this.id, this.value)"></td>
 				</tr>
 				<tr>
-					<td><h5>E-Mail</h5></td>
-					<td><input type="text" name="E-Mail" title="E-Mail" id="E-Mail" required="true"></td>
+					<td><h5>E-Mail *</h5></td>
+					<td><input type="text" name="E-Mail" title="E-Mail" id="E-Mail" required="true" onblur="emailValidator(this.id, this.value)"></td>
 				</tr>
 				<tr>
-					<td><h5>CAP</h5></td>
+					<td><h5>CAP *</h5></td>
 					<td><input type="text" name="CAP" title="CAP" id="CAP" required="true" 
 					onblur="capValidation(this.id, this.value)"></td>
 				</tr>
 				<tr>
-					<td><h5>Num. Civico</h5></td>
+					<td><h5>Num. Civico *</h5></td>
 					<td><input type="text" name="No-Civico" title="No-Civico" id="No-Civico" required="true" onblur="civicNumberValidation(this.id, this.value)"></td>
 				</tr>
 				<tr>
-					<td><h5>Via</h5></td>
+					<td><h5>Via *</h5></td>
 					<td><input type="text" name="via" title="via" id="via" required="true" onblur="textValidation(this.id, this.value)"></td>
 				</tr>	
 				<tr>
-					<td><h5>Telefono</h5></td>
-					<td><input type="text" name="telefono" title="telefono" id="telefono" required="true"></td	>
+					<td><h5>Telefono *</h5></td>
+					<td><input type="text" name="telefono" title="telefono" id="telefono" required="true" onblur="phoneValidator(this.id, this.value)"></td>
 				</tr>	
 				<tr>
-					<td><h5>Sesso</h5></td>
-					<td><input type="radio" name="sesso" value="uomo">Uomo
-					<input type="radio" name="sesso" value="donna">Uomo
+					<td><h5>Sesso *</h5></td>
+					<td><input type="radio" name="sesso" value="uomo" checked="true" onblur="enableButton()">Uomo
+					<input type="radio" name="sesso" value="donna">donna
 					<input type="radio" name="sesso" value="altro">Altro</td>
 				</tr>	
 				<tr>
 					<td><h5>Hobby</h5></td>
-					<td><input type="text" name="hobby" title="hobby" id="hobby" onblur="checkLenght(this.id, this.value)"></td>
+					<td><input type="text" name="hobby" title="hobby" id="hobby" onblur="checkLength(this.id, this.value)"></td>
 				</tr>
 				<tr>
 					<td><h5>Professione</h5></td>
-					<td><input type="text" name="professione" title="professione" id="	professione"onblur="checkLenght(this.id, this.value)"></td>
+					<td><input type="text" name="professione" title="professione" id="	professione"onblur="checkLength(this.id, this.value)"></td>
 				</tr>
 				<tr>
-					<td><input type="submit" name="avanti" value="Avanti" id="avanti" onclick="disableInputs()"></td>
-					<td><a href="register.php"><input type="button" name="correggi" value="Cancella"></a></td>
+					<td><a href="register.php"><input type="button" name="cancella" id="cancella" value="Cancella"></a></td>
+					<td><input type="button" name="avanti" value="Avanti" id="avanti" onclick="disableInputs()"></td>
+					
 				</tr>	
 			</table>
 		</form>
